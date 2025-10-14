ef9fb3e21c6c8c065d5990d9391186ae99b94913
SophiaCode123 <sofie.rgg@gmail.com>
2025-10-14 13:15:28 -0400

    Update Part_1.py

--- Part_1.py
+++ Part_1.py
@@ -1,11 +1,8 @@
 print("Welcome to the work planner! Please log your work down below.")
-def takes_input():
-    week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    assignments = []
-    for day in week:
-        daily_task = input(f"What do you have to do for {day}:")
-        time = input("How much time will it take?(in minutes)")
-        assignments.append([day, daily_task, time])
-    return assignments
-print(takes_input())
+week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+assignments = []
+for day in week:
+    time = input(f"How much time your task it take?(in minutes) on {day}: ")
+    assignments.append([day, time])
+
 
