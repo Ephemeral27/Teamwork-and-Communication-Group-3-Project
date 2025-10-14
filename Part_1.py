34b35dc4e6af8f50c715b32ae1c13e346a27e587
SophiaCode123 <sofie.rgg@gmail.com>
2025-10-14 13:20:08 -0400

    Update Part_1.py

--- Part_1.py
+++ Part_1.py
@@ -1,8 +1,14 @@
 print("Welcome to the work planner! Please log your work down below.")
+
 week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
 assignments = []
+
 for day in week:
+
     time = input(f"How much time your task it take?(in minutes) on {day}: ")
+    
+
     assignments.append([day, time])
 
-
+print(assignments)
